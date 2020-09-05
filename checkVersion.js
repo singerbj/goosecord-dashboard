bfff1d729d7949961950ae27d4235208b5152a0d
Benjamin Singer <benjaminjsinger@gmail.com>
2020-09-04 21:59:50 -0500

    remove electron and all that crap

--- checkVersion.js
+++
@@ -1,8 +0,0 @@
-const semver = require('semver');
-const packageJson = require('./package');
-
-const version = packageJson.engines.node;
-if (!semver.satisfies(process.version, version)) {
-  console.log(`Required node version ${version} not satisfied with current version ${process.version}.`);
-  process.exit(1);
-}
